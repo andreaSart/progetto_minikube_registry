a563c34775ce724fb9d90260cf7db065927cc693
Andrea Sartirana <asartirana@SITRADE.ITALIA>
2023-03-12 00:24:52 +0100

    simple example dockerfile

---
+++ localregistrytest/src/main/java/com/example/localregistrytest/LocalregistrytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.localregistrytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalregistrytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalregistrytestApplication.class, args);
+	}
+
+}
---
+++ localregistrytest/src/test/java/com/example/localregistrytest/LocalregistrytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localregistrytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalregistrytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
